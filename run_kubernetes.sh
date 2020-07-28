64f685d5613d976d24d3d04bdcbaa3bbe6cca656
Aditya <aditya@imac-van-eddy.home>
2020-07-28 16:38:46 +0200

    Move output files to correct folder

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=ashekker/udacitymicroservices
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitymicroservices\
+    --image=$dockerpath\
+    --port=80 --labels app=udacitymicroservices
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacitymicroservices 8000:80
