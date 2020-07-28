7476e013858cef394a47dfb7719bcbc733d23c52
Aditya <aditya@imac-van-eddy.home>
2020-07-28 16:13:58 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=ashekker/udacitymicroservices
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker login && docker image tag udacitymicroservices $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker image push $dockerpath
